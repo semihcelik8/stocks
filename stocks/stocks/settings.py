4a962754a5a69c1ee02516b9091e1a96527c0ab8
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2023-05-25 22:25:02 +0300

    Update settings.py

--- stocks/stocks/settings.py
+++ stocks/stocks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cdqc!e=7+z5#&(eyfd+9mve4@7yv1)t!62^kq61mc_&e($52!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
